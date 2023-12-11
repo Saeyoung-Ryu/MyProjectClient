3a38a34f1cf4315b4465706638532cb7c197a3c5
Saeyoung-Ryu <usaeyoung9514@gmail.com>
2023-12-11 16:43:54 +0900

    1211_1

--- BlazorApp3/obj/Debug/net7.0/BlazorApp3.AssemblyInfo.cs
+++ BlazorApp3/obj/Debug/net7.0/BlazorApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b6e217fc373706a66c873f8f67405d5facc893")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2d1f7d3951dbd3731582b4183031d78c8cc4bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
