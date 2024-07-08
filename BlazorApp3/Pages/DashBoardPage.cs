621252036cce2174c8713c43eba54834fb3acdf3
유세영 <usaeyoung9514@gmail.com>
2024-07-08 17:54:26 +0900

    708

--- BlazorApp3/Pages/DashBoardPage.cs
+++ BlazorApp3/Pages/DashBoardPage.cs
@@ -1,4 +1,6 @@
+using MatBlazor;
 using Microsoft.AspNetCore.Components;
+using Protocol.Type;
 
 namespace BlazorApp3.Pages;
 
@@ -8,6 +10,21 @@ public partial class DashBoardPage
     public string DashBoardName { get; set; }
     
     public int tabIndex = 0;
+    public int rankTabIndex = 0;
+    private bool showEditModal = false;
+    
+    public DashBoardInfo dashBoardInfo = new DashBoardInfo();
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        dashBoardInfo = await GetDashBoardInfoAsync();
+
+        if (dashBoardInfo == null)
+        {
+            ShowError(MatToastType.Warning, "DashBoard Not Found");
+        }
+    }
 
     protected override void OnInitialized()
     {
@@ -37,5 +54,65 @@ public partial class DashBoardPage
             
             // dashBoardName 존재하는지 찾기
         }
+    }
+
+    private async Task<DashBoardInfo> GetDashBoardInfoAsync()
+    {
+        return dashBoardInfo = new DashBoardInfo()
+        {
+            DashBoardSeq = 1,
+            Name = DashBoardName,
+            CreateTime = DateTime.UtcNow,
+            MasterName = "Master1",
+            ManagerNameList = new List<string>()
+            {
+                "Manager1",
+                "Manager2",
+                "Manager3",
+            },
+            MemberTotalRankOrderByName = new List<string>()
+            {
+                "Member1",
+                "Member2",
+                "Member3",
+            },
+            MemberSupRankOrderByName = new List<string>()
+            {
+                "Member1",
+                "Member2",
+                "Member3",
+            },
+            MemberAdcRankOrderByName = new List<string>()
+            {
+                "Member1",
+                "Member2",
+                "Member3",
+            },
+            MemberMidRankOrderByName = new List<string>()
+            {
+                "Member1",
+                "Member2",
+                "Member3",
+            },
+            MemberJgRankOrderByName = new List<string>()
+            {
+                "Member1",
+                "Member2",
+                "Member3",
+            },
+            MemberTopRankOrderByName = new List<string>()
+            {
+                "Member1",
+                "Member2",
+                "Member3",
+            },
+            Point = 100,
+            Notification = "Notification Example 1234",
+        };
+    }
+    
+    private void ShowEditModal()
+    {
+        showEditModal = true;
     }
 }
